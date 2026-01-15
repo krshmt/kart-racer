706d370f23c2d8cf5e11b14c4d5c174b87fc6a88
krshmt <tourekris16@gmail.com>
2026-01-15 15:01:22 +0100

    + lenis scroll +animation au scroll avec rotation de l'image + correction de la fin de l'animation

--- src/components/scroll-image/scroll-image.jsx
+++ src/components/scroll-image/scroll-image.jsx
@@ -1,16 +1,54 @@
+import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./index.css";
 
 function ScrollImage() {
+  const sectionRef = useRef(null);
+  const containerRef = useRef(null);
+  const imageRightRef = useRef(null);
+  const imageLeftRef = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const ctx = gsap.context(() => {
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: sectionRef.current,
+            start: "top top",
+            end: "bottom bottom",
+            scrub: true,
+            markers: true,
+          },
+        })
+        .to(
+          imageRightRef.current,
+          { left: "70%", rotation: 8, ease: "none" },
+          0
+        )
+        .to(
+          imageLeftRef.current,
+          { left: "30%", rotation: -8, ease: "none" },
+          0
+        );
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <>
-      <div className="h-100"></div>
-      <div className="images-container">
-        <img src="./public/images/image-2.png" alt="" />
-        <img src="./public/images/image-3.png" alt="" />
-        <img src="./public/images/image-4.png" alt="" />
+      <div className="h-50"></div>
+      <div className="scroll-images-section" ref={sectionRef}>
+        <div className="images-container" ref={containerRef}>
+          <img src="./public/images/image-2.png" alt="" />
+          <img ref={imageRightRef} src="./public/images/image-3.png" alt="" />
+          <img ref={imageLeftRef} src="./public/images/image-4.png" alt="" />
+        </div>
       </div>
-      <div className="h-100"></div>
+      <div className="h-50"></div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Lenis from 'lenis'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,3 +9,15 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+const lenis = new Lenis({
+  duration: 1.1,
+  smoothWheel: true,
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
