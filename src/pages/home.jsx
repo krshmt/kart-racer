3f68808ee8998de177c0363c9ea4cf99737077b5
krshmt <tourekris16@gmail.com>
2026-01-27 11:43:51 +0100

    feat: add Footer component with shader effect and styling
    - Implemented Footer component with dynamic shader effect using Three.js
    - Added operating hours display with a responsive layout
    - Included social media icons for contact section
    - Created accompanying CSS styles for layout and design

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Carrousel from "../components/Carrousel/carrousel";
 import ScrollImage from "../components/scroll-image/scroll-image";
+import Footer from "../components/Footer/footer";
 
 function Home() {
     return (
         <>
             <ScrollImage />
             <Carrousel />
-            <div className="h-25"></div>
+            <Footer />
         </>
     );
 }
